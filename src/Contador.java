97e058336a00141dd8fca12caae3be918be61eb7
tamiresboldarim <tamiresboldarim@hotmail.com>
2024-06-06 00:37:52 -0300

    tentativa 2

--- src/Contador.java
+++ src/Contador.java
@@ -1,8 +1,11 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Contador {
 	public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
+        Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+	
 		    
         System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = terminal.nextInt();
@@ -10,25 +13,29 @@ public class Contador {
         System.out.println("Digite o segundo parâmetro");
 		int parametroDois = terminal.nextInt();
 
+		
+
 		try {
 			//chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
-
-		}catch (java.util.InputMismatchException e) {
+			
+		}catch (InputMismatchException e) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-            System.err.println("O segundo parâmetro deve ser maior que o primeiro.");
+            System.out.println(e.getMessage());
 		}
 		
 	}
 
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		} else{
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
-        for( ; parametroUm <=parametroDois; ) {
+        for(parametroUm = 0; parametroUm <= parametroDois; parametroUm++ ) {
             System.out.println(parametroUm + " - parâmetro(s)");
-            parametroUm ++;
+		}  
        }
 		
     }
--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,3 +1,5 @@
-public class ParametrosInvalidosException {
-    
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+		super("O segundo parâmetro deve ser maior que o primeiro");
+    }
 }
